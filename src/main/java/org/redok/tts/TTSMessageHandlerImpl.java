a3e4f2a7abf1d19f44ab8916972403dc24cf28da
RedokPaw <proredok125@gmail.com>
2026-04-13 18:09:39 +0300

    fixed critical bug

--- src/main/java/org/redok/tts/TTSMessageHandlerImpl.java
+++ src/main/java/org/redok/tts/TTSMessageHandlerImpl.java
@@ -91,8 +91,6 @@ public class TTSMessageHandlerImpl implements TTSMessageHandler {
                 audioStream.transferTo(ffmpegIn);
             } catch (IOException e) {
                 log.warning("Error writing to ffmpeg stdin");
-            } finally {
-                process.destroy();
             }
         });
         return new BufferedInputStream(process.getInputStream());
